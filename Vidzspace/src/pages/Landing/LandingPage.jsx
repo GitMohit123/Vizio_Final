e248db1831a3337baecd69a55aef5dcb68f89298
GitMohit123 <jindalm55@gmail.com>
2024-07-13 23:21:38 +0530

    Landing Page added

--- Vidzspace/src/pages/Landing/LandingPage.jsx
+++ Vidzspace/src/pages/Landing/LandingPage.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div className=''>LandingPage</div>
+    <div className='landing-page' style={{ height: '100vh', width: '100%' }}>
+      <iframe 
+        src="https://vizio.framer.website/" 
+        style={{ border: 'none', width: '100%', height: '100%' }} 
+        title="Framer Website"
+      />
+    </div>
   )
 }
 
